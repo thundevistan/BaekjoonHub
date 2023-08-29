49a605deaf83694e0e1edc2594436b1ef8ce0ed1
thundevistan <139092551+thundevistan@users.noreply.github.com>
2023-08-29 14:43:36 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 884 ms, Memory: 293836 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
@@ -0,0 +1,16 @@
+package com.kotdev99.android.algorithm
+
+import java.util.StringTokenizer
+
+fun main(args: Array<String>) = with(System.`in`.bufferedReader()) {
+	val bw = System.out.bufferedWriter()
+
+	repeat(readLine().toInt()) {
+		val st = StringTokenizer(readLine())
+
+		val sum = (st.nextToken().toInt() + st.nextToken().toInt()).toString()
+		bw.write(sum + "\n")
+	}
+	close()
+	bw.close()
+}
